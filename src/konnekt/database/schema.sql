e2f9f3f99d9b6b469438870b2e4c842215282979
Rajesh Thapa <devrajeshthapa@gmail.com>
2025-12-15 08:41:13 +0545

    add schema script for database

---
+++ src/konnekt/database/schema.sql
@@ -0,0 +1,21 @@
+/*
+    Usage:
+        First way:
+            mysql -u root -p < <path_to_this_file>
+
+        Second way:
+            mysql -u root -p
+            source <path_to_this_file>
+*/
+
+CREATE DATABASE IF NOT EXISTS konnekt;
+USE konnekt;
+
+CREATE TABLE user (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    full_name VARCHAR(100) NOT NULL,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
