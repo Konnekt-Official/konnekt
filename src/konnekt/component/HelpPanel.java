1d716b3a2b53377d71cfe3fcaa7df3382f1c3379
Samjhana <samjhanadahal2000@gmail.com>
2025-12-30 12:00:00 +0545

    add help panel

---
+++ src/konnekt/component/HelpPanel.java
@@ -0,0 +1,37 @@
+package konnekt.component;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+
+public class HelpPanel extends JPanel {
+
+    public HelpPanel() {
+        setLayout(new BorderLayout());
+        setBackground(Color.WHITE);
+        setBorder(new EmptyBorder(30, 30, 30, 30));
+
+        JLabel title = new JLabel("Help & Support");
+        title.setFont(new Font("Verdana", Font.BOLD, 22));
+
+        JTextArea text = new JTextArea(
+            "If you need assistance while using Konnekt, we are here to help.\n\n" +
+
+            "You can find answers to common questions within the app or " +
+            "contact our support team for further assistance.\n\n" +
+
+            "Support Email: rajeshthapa.cs@gmail.com\n" +
+            "Response time: within 24–48 hours."
+        );
+
+        text.setFont(new Font("Verdana", Font.PLAIN, 14));
+        text.setBorder(new EmptyBorder(20, 0, 0, 0));
+        text.setEditable(false);
+        text.setLineWrap(true);
+        text.setWrapStyleWord(true);
+        text.setBackground(Color.WHITE);
+
+        add(title, BorderLayout.NORTH);
+        add(text, BorderLayout.CENTER);
+    }
+}
