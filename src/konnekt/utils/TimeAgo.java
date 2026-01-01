e6ca7be888f6038a0322ffc2d9cdcab768b383cb
Rajesh Thapa <devrajeshthapa@gmail.com>
2026-01-01 23:28:11 +0545

    Update TimeAgo.java

--- src/konnekt/utils/TimeAgo.java
+++ src/konnekt/utils/TimeAgo.java
@@ -8,9 +8,9 @@ public class TimeAgo {
     public static String format(Timestamp t) {
         long sec = Duration.between(t.toInstant(), java.time.Instant.now()).getSeconds();
 
-        if (sec < 60) return sec + "s ago";
-        if (sec < 3600) return (sec / 60) + "m ago";
-        if (sec < 86400) return (sec / 3600) + "h ago";
-        return (sec / 86400) + "d ago";
+        if (sec < 60) return "• Just Now";
+        if (sec < 3600) return "• " + sec / 60 + "m";
+        if (sec < 86400) return "• " + sec / 3600 + "h";
+        return "• " + sec / 86400 + "d";
     }
 }
